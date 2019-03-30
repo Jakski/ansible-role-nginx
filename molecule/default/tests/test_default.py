a6c7315244428c9eba4ad31714381ff78fda9a6c
Jakub Pieńkowski <8525083+Jakski@users.noreply.github.com>
2019-03-30 13:01:45 +0100

    Change templates

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -16,18 +16,16 @@ def test_service(host):
 
 def test_configuration(host):
     assert not host.file('/etc/nginx/sites-enabled/default').exists
-    assert host.file('/etc/nginx/nginx.conf').contains(
+    main = host.file('/etc/nginx/nginx.conf')
+    assert main.contains(
         r'^include /etc/nginx/modules-enabled/\*\.conf;$')
-    assert host.file('/etc/nginx/nginx.conf').contains(
-        r'server_names_hash_bucket_size 128;$')
-    assert host.file('/etc/nginx/sites-enabled/example1.conf').contains(
-        r'root /var/www/html;$')
-    assert host.file('/etc/nginx/sites-enabled/example1.conf').contains(
-        r'server_name www.test.local;$')
-    assert host.file('/etc/nginx/sites-enabled/example2.conf').contains(
-        r'root /var/www/html;$')
-    assert host.file('/etc/nginx/sites-enabled/example2.conf').contains(
-        r'server_name _;$')
+    assert main.contains(r'server_names_hash_bucket_size 128;$')
+    example1 = host.file('/etc/nginx/sites-enabled/example1.conf')
+    assert example1.contains(r'access_log /var/log/nginx/example1-access.log;$')
+    assert example1.contains(r'server_name www.test.local;$')
+    example2 = host.file('/etc/nginx/sites-enabled/example2.conf')
+    assert example2.contains(r'access_log /var/log/nginx/example2-access.log;$')
+    assert example2.contains(r'server_name _;$')
     assert not host.file('/etc/nginx/modules-enabled/50-mod-mail.conf').exists
     assert host.file('/etc/nginx/modules-enabled/50-mod-test.conf').exists
     assert host.file('/etc/nginx/conf.d/maps.conf').contains(
